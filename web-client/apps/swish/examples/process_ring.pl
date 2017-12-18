3224a22324ab9f67536e9aa9f1d92a5aa757e3c9
Torbjörn Lager <torbjorn.lager@ling.gu.se>
2017-12-18 08:56:58 +0100

    Added process ring to examples.

--- web-client/apps/swish/examples/process_ring.pl
+++ web-client/apps/swish/examples/process_ring.pl
@@ -1,33 +1,29 @@
-:- use_module(web_prolog).
 
-s(N, Msg) :-
-    spawn(start(N, Msg), _Pid, [
-        src_text("
-    
-            start(NumberProcesses, Message) :-
-                get_time(Start),
-                self(Self),
-                create(NumberProcesses, Self, Message),
-                receive({ Msg ->
-                          get_time(End),
-                          Wall is End - Start,
-                          writeln(Msg-Wall) }).
+start(NumberProcesses, Message) :-
+    get_time(Start),
+    self(Self),
+    create(NumberProcesses, Self, Message),
+    receive({ 
+    	Msg ->
+        	get_time(End),
+        	Wall is End - Start,
+            echo(Msg-Wall)
+     }).
+              
+create(1, NextProcess, Message) :- !,
+    NextProcess ! Message.
+create(NumberProcesses, NextProcess, Message) :-
+    spawn(loop(NextProcess), Prev,
+          [ link(true),
+            src_predicates([loop/1])
+          ]),
+    NumberProcesses1 is NumberProcesses - 1,
+    create(NumberProcesses1, Prev, Message).
 
-            create(1, NextProcess, Message) :- !,
-                NextProcess ! Message.
-            create(NumberProcesses, NextProcess, Message) :-
-                spawn(loop(NextProcess), Prev,
-                      [ link(true)
-                      ]),
-                NumberProcesses1 is NumberProcesses - 1,
-                create(NumberProcesses1, Prev, Message).
-
-            loop(NextProcess) :-
-                receive({
-                    Msg ->
-                        NextProcess ! Msg
-                }).
-        ")
-    ]).
+loop(NextProcess) :-
+    receive({
+        Msg ->
+            NextProcess ! Msg
+    }).
 
 
